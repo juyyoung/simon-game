f236231fe62cbf82b59820e9063100932a6e267f
juyyoung <76234653+juyyoung@users.noreply.github.com>
2021-03-07 14:10:20 +0900

    finish

--- game.js
+++ game.js
@@ -1,26 +1,73 @@
-const gamePattern = [];
-
-const userClickedPattern = [];
+let gamePattern = [];
+let userClickedPattern = [];
 
 const buttonColours = ["red", "blue", "green", "yellow"];
+
+let started = false;
+
+let level = 0;
+
+
+$(document).keydown(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+})
 
 $(".btn").click(function() {
   let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
-
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
 })
 
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+      }
+
+    } else {
+
+      $("body").addClass("game-over");
+      setTimeout(function() {
+        $("body").removeClass("game-over")
+      }, 200);
+
+      playSound("wrong");
+      $("#level-title").text("Game over, Press Any Key to Restart")
+
+      startOver();
+
+    }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
   playSound(randomChosenColour);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
 
 function playSound(name) {
@@ -30,7 +77,6 @@ function playSound(name) {
 
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
-
   setTimeout(function() {
     $("#" + currentColour).removeClass("pressed");
   }, 100);
